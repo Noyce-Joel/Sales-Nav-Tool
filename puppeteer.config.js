d3809c1f4eefa0a3cf5c42904d8d744be44fb56e
Joel Noyce <noyce.joel@gmail.com>
2024-10-01 10:52:15 +0100

    added a puppeteer config file

---
+++ puppeteer.config.js
@@ -0,0 +1,16 @@
+import { join } from "path";
+
+/**
+ * @type {import('puppeteer').Configuration}
+ */
+module.exports = {
+  chrome: {
+    skipDownload: false,
+  },
+
+  firefox: {
+    skipDownload: true,
+  },
+
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
