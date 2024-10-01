821c59f03af767dca2228d844276fe23c75a7452
Joel Noyce <noyce.joel@gmail.com>
2024-10-01 10:54:33 +0100

    used rrequire in config to resolve error

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -1,14 +1,22 @@
-import { join } from "path";
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { join } = require('path');
 
 /**
  * @type {import('puppeteer').Configuration}
  */
 module.exports = {
+  // Download and use the default Chrome version.
   chrome: {
-    skipDownload: false,
+    skipDownload: false,  // Ensure Chrome is downloaded if not already
   },
+  
+  // If you want to support Firefox (cross-browser testing)
   firefox: {
-    skipDownload: true,
+    skipDownload: true,  // You can set this to false to download Firefox too
   },
-  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  
+  // Customize the cache directory (helpful for environments with build steps)
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+
+  // Optional: You could set default args or env settings if needed
 };
